9db871ab3cf2390fc68f83c70035ec44c81a0db5
HangOn6 <66875478+HangOn6@users.noreply.github.com>
2020-06-13 22:34:50 +0530

    Grayscale and Delta Difference updated
    Naming conventions for imshow windows

--- Motiondetector.py
+++ Motiondetector.py
@@ -16,14 +16,14 @@ while True:
     check, frame=video.read()
     status=0
    
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray=cv2.GaussianBlur(gray,(21,21),0)
+    grayscale=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    grayscale=cv2.GaussianBlur(grayscale,(21,21),0)
    
     if first_frame is None:
-        first_frame=gray
+        first_frame=grayscale
         continue
 
-    delta_frame=cv2.absdiff(first_frame,gray)   
+    delta_frame=cv2.absdiff(first_frame,grayscale)   
     thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
     thresh_frame=cv2.dilate(thresh_frame,None,iterations=2)
 
@@ -47,10 +47,10 @@ while True:
         times.append(datetime.now())
 
 
-    cv2.imshow("Gray Frame",gray)
-    cv2.imshow("Delta Frame",delta_frame)
+    cv2.imshow("GrayScale Frame",grayscale)
+    cv2.imshow("Delta Difference Frame",delta_frame)
     cv2.imshow("Threshold Frame",thresh_frame)
-    cv2.imshow("Color Frame",frame)
+    cv2.imshow("Original Colour Frame",frame)
 
     key=cv2.waitKey(1)
 
